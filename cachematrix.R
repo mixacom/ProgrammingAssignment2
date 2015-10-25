f4f7fb9c91e63c53cc020f58b6118d72ff0d67c6
mixacom <wmmix@mail.ru>
2015-10-25 19:20:40 +0100

    Solution for the second assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## Function makeCacheMatrix creates a new environment that stores an initial matrix, invariant matrix and provides functions to
+## set and get these matrices 
 
 makeCacheMatrix <- function(x = matrix()) {
+    originalMatrix <- x 
+    invMatrix <- NULL
+    
+    ## set a new initial matrix 
+    set <- function(y) {
+        originalMatrix <<- y
+        invMatrix <<- NULL
+    }
+
+    ## get an initial matrix 
+    get <- function() {
+        originalMatrix
+    }
+
+    ## set an inverted matrix 
+    setInvMatrix <- function(sMatrix) {
+        invMatrix <<- sMatrix 
+    }    
+
+    ## get an inverted matrix 
+    getInvMatrix <- function() {
+        invMatrix
+    }
+    
+    list( set = set, get = get, 
+          setInvMatrix = setInvMatrix, 
+          getInvMatrix = getInvMatrix)
+} 
  
-}
 
-
-## Write a short comment describing this function
-
+## This function returns the cached value of inverted matrix or computes new invertef matrix  
+    
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+
+    ## attempt to get cached value for an inveted matrix
+    invMatrix <- x$getInvMatrix()
+     if (!is.null(invMatrix)) {
+        message("getting cached data")
+        return(invMatrix)
+    }
+    
+    ## if no cached inverted matrix then compute a new one 
+    data <- x$get()
+    invMatrix <- solve(data, ...)
+    x$setInvMatrix(invMatrix)
+    invMatrix
+} 
